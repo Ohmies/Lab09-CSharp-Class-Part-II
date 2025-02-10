a705533e6129ba9ab155d705de130fb6bd830054
Ohmies <Ohmperapatsingpan.15@gmail.com>
2025-02-10 22:46:38 +0700

    Update 1

---
+++ Lab09_Ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿Person.name = "OHM";
+System.Console.WriteLine(Person.Getname());
+
+static class Person
+{
+    public static string name;
+    public static string Getname()
+    {
+        return $"Hello from {name}";
+    }
+}
