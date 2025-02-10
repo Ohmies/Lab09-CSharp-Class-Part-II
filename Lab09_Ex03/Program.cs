433d730862577f8d0bcc74d96425042a8e185a1e
Ohmies <Ohmperapatsingpan.15@gmail.com>
2025-02-10 23:19:50 +0700

    Update

---
+++ Lab09_Ex03/Program.cs
@@ -0,0 +1,23 @@
+﻿Person.name = "Perapat Singpan";
+System.Console.WriteLine(Person.Getname());
+var student = new Student();
+Student.name = "Perapat Singpan";
+System.Console.WriteLine(student.Getname());
+
+static class Person
+{
+    static public string name;
+    static public string Getname()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student
+{
+    public static string name;
+    public string Getname()
+    {
+        return $"Hello from {name}";
+    }
+}
